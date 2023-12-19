976588ed3d8f86e3deb5cfbb073089869d6b540e
Carlitos <carlitosdutan@pursuit.org>
2023-12-19 11:22:47 -0500

    updated header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>Basic Pursuit React Starter</div>;
+  return (
+    <div style={{ color: "blue", textAlign: "center" }}>
+      <h1>Deployed React Basic Starter</h1>
+    </div>
+  );
 };
 
 export default App;
